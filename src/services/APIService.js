052f94d82da0b9f210ec3136d4db7939e7e519fa
EAguirre <aguirreernesto25@gmail.com>
2023-08-02 11:52:17 -0300

    Recetas

--- src/services/APIService.js
+++ src/services/APIService.js
@@ -4,7 +4,10 @@ export default {
     obtenerCategorias() {
         return api('/list.php?c=list')
     },
-    buscarRecetas ({categoria, nombre}) {
+    buscarRecetas({categoria, nombre}) {
         return api(`/filter.php?c=${categoria}&i=${nombre}`)
+    },
+    buscarReceta(id) {
+        return api(`/lookup.php?i=${id}`)
     }
 }
