93d16eef9773244cb777ce1e5542d0fb9acb32ec
EAguirre <aguirreernesto25@gmail.com>
2023-08-02 12:18:28 -0300

    Store creado y listo

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
   import Header from './components/Header.vue';
   import { RouterView } from 'vue-router'; 
+  import Modal from './components/Modal.vue';
 </script>
 
 <template>
@@ -9,4 +10,7 @@
   <main class="container mx-auto mt-10">
     <RouterView/>
   </main>
+
+  <Modal/>
+  
 </template>
