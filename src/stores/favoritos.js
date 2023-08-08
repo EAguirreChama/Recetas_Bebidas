d9958fa435f670e52c1001844fdce842b9318a8d
EAguirre <aguirreernesto25@gmail.com>
2023-08-08 17:19:00 -0300

    Mensaje de que no hay favoritos

--- src/stores/favoritos.js
+++ src/stores/favoritos.js
@@ -1,4 +1,4 @@
-import { ref, watch, onMounted } from "vue";
+import { ref, watch, onMounted, computed } from "vue";
 
 import { defineStore } from "pinia";
 import { useBebidasStore } from "./bebidas";
@@ -46,8 +46,11 @@ export const useFavoritosStore = defineStore('favoritos', () => {
         modal.modal = false
     }
 
+    const noFavoritos = computed(() => favoritos.value.length === 0)
+
     return {
         favoritos,
+        noFavoritos,
         existeFavorito,
         handleClickFavoritos
     }
