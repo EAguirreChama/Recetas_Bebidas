c6db7168e6ad059a6c79b6c70724fe3e8952027d
EAguirre <aguirreernesto25@gmail.com>
2023-08-08 17:13:07 -0300

    Eliminar correctamente de favoritos

--- src/stores/favoritos.js
+++ src/stores/favoritos.js
@@ -2,10 +2,12 @@ import { ref, watch, onMounted } from "vue";
 
 import { defineStore } from "pinia";
 import { useBebidasStore } from "./bebidas";
+import { useModalStore } from "./modal";
 
 export const useFavoritosStore = defineStore('favoritos', () => {
 
     const bebidas = useBebidasStore()
+    const modal = useModalStore()
     const favoritos = ref([])
 
     onMounted (() => {
@@ -22,17 +24,26 @@ export const useFavoritosStore = defineStore('favoritos', () => {
         localStorage.setItem('favoritos', JSON.stringify(favoritos.value))
     }
 
-    function existeFavorito (id) {
+    function eliminarFavorito () {
+        favoritos.value = favoritos.value.filter( favorito => favorito.idDrink !== bebidas.receta.idDrink )
+    }
+
+    function agregarFavorito () {
+        favoritos.value.push(bebidas.receta)
+    }
+
+    function existeFavorito () {
         const favoritosLocalStorage = JSON.parse(localStorage.getItem('favoritos')) ?? []
-        return favoritosLocalStorage.some(favorito => favorito.idDrink === id)
+        return favoritosLocalStorage.some(favorito => favorito.idDrink === bebidas.receta.idDrink)
     }
 
     function handleClickFavoritos () {
-        if(existeFavorito(bebidas.receta.idDrink)) {
-            console.log('Ya existe...');
+        if(existeFavorito()) {
+            eliminarFavorito()
         } else {
-            favoritos.value.push(bebidas.receta)
+            agregarFavorito()
         }
+        modal.modal = false
     }
 
     return {
