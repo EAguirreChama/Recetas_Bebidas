80533942033096905372c174c0f5e3fad64bfdfa
Ernesto Aguirre <125767165+EAguirreChama@users.noreply.github.com>
2023-09-13 13:09:51 -0300

    Update FavoritosView.vue

--- src/views/FavoritosView.vue
+++ src/views/FavoritosView.vue
@@ -1,15 +1,10 @@
 <script setup>
-    
     import Recetas from '../components/Recetas.vue'
-    
     import { useFavoritosStore } from '../stores/favoritos';
-
     const favoritos = useFavoritosStore()
-
 </script>
 
 <template>
-
     <h1 class="text-6xl font-extrabold text-center">Favoritos</h1>
 
     <p v-if="favoritos.noFavoritos" class="my-10 text-center text-2xl">No hay Favoritos</p>
@@ -21,5 +16,4 @@
             :key="receta.idDrink"
         />
     </div>
-
 </template>
