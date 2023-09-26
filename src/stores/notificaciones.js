71647fb3802f3b04b1fe4a71f8d44b8cca24ec12
Ernesto Aguirre <125767165+EAguirreChama@users.noreply.github.com>
2023-09-26 19:24:34 -0300

    Update notificaciones.js

--- src/stores/notificaciones.js
+++ src/stores/notificaciones.js
@@ -1,5 +1,5 @@
-import { ref, watch } from "vue";
-import { defineStore } from "pinia";
+import { ref, watch } from "vue"
+import { defineStore } from "pinia"
 
 export const useNotificacionStore = defineStore('notificacion', () => {
 
