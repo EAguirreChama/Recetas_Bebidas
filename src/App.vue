c6e58a2124fafcf3b26d97eefb054a3fddbddd0d
Ernesto Aguirre <125767165+EAguirreChama@users.noreply.github.com>
2023-09-26 19:25:38 -0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <script setup>
-  import Header from './components/Header.vue';
-  import { RouterView } from 'vue-router'; 
-  import Modal from './components/Modal.vue';
-  import Notificacion from './components/Notificacion.vue';
+  import Header from './components/Header.vue'
+  import { RouterView } from 'vue-router'
+  import Modal from './components/Modal.vue'
+  import Notificacion from './components/Notificacion.vue'
 </script>
 
 <template>
